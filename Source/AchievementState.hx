de6de2530266f6b2e686a2a277744639f1aca35f
MMbay64 <115948389+MMbay64@users.noreply.github.com>
2022-10-29 06:24:52 -0400

    Initial commit

---
+++ Source/AchievementState.hx
@@ -0,0 +1,13 @@
+package;
+
+
+class achievementstate {
+  var achievements:array<string> = ["Just the beginning","complete week 1 with no misses"],
+                                   ["SPOOPY MONTH!!","complete week 2 with no misses"],
+                                   ["Go Pico!!","complete week 3 with no misses "],
+                                   ["Heat waves...","complete week 4 with no misses"],
+                                   ["Christmas is ruined...","complete week 5 with no misses"],
+                                   ["we just skipped high-school","complete week 6 with no misses"],
+                                   ["UGHHHH!!","complete week 7 with a accuracy of 20.6%"],
+                                   ["ESCAPER!!","unlock 'bichrome' on hard"]
+           
